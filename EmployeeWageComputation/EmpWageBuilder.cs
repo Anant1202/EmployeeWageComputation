3a8783f6c45d32b931e4aee41ea2e8b1122418c7
Anant1202 <anantsingh1202@gmail.com>
2022-05-16 16:05:56 +0530

    Added:Total Wage For Each Company

---
+++ EmployeeWageComputation/EmpWageBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmpWageBuilder
+    {
+        public const int Is_PartTime = 1;
+        public const int Is_FullTime = 2;
+
+        public string company;
+        public int Emp_Wage_Per_Hour;
+        public int Working_days;
+        public int Total_Working_Hours;
+        public int totalwage;
+
+        public EmpWageBuilder(string company, int Emp_Wage_Per_Hour, int Working_days, int Total_Working_Hours)
+        {
+            this.company = company;
+            this.Emp_Wage_Per_Hour = Emp_Wage_Per_Hour;
+            this.Working_days = Working_days;
+            this.Total_Working_Hours = Total_Working_Hours;
+        }
+        public void CalculateEmpWage()
+        {
+            int emphrs = 0, day = 1, totalhours = 0;
+
+            while (totalhours <= this.Total_Working_Hours && day <= this.Working_days)
+            {
+
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case Is_PartTime:
+                        emphrs = 4;
+                        break;
+                    case Is_FullTime:
+                        emphrs = 8;
+                        break;
+
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+                totalhours += emphrs;
+                Console.WriteLine("Day: " + day + "Emp Hrs:" + emphrs);
+                day++;
+            }
+            totalwage = totalhours * Emp_Wage_Per_Hour;
+            Console.WriteLine("Total Employee Wage for Company " + company + " is " + totalwage);
+        }
+    }   
+}
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,17 +7,19 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmpWageComputation emp = new EmpWageComputation();
-            emp.MultipleCompanies("Google", 20, 5, 10);
-            emp.MultipleCompanies("Apple", 15, 4, 15);
-
-
+            EmpWageBuilder Google = new EmpWageBuilder("Google", 40, 25, 400);
+            Google.CalculateEmpWage();
+            
+            EmpWageBuilder Apple = new EmpWageBuilder("Apple", 300, 15, 300);
+            Apple.CalculateEmpWage();
+           
+            //EmpWageComputation emp = new EmpWageComputation();
+            //emp.MultipleCompanies("Google", 20, 5, 10);
+            //emp.MultipleCompanies("Apple", 15, 4, 15);
             //emp.EmployeeWageUsingSwitch();
             //emp.CheckEmpPresentOrNot();
             //emp.CheckEmpFullOrPartTime();
             //emp.CalcEmpDailyWage();
-
-
         }
     }
 }
